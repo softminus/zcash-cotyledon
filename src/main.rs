e90e850f05c4a881591f92a6c4d7e181caf850a5
sasha <sasha@z.cash>
2022-12-07 14:21:37 -0800

    use the ancillary checks

--- src/main.rs
+++ src/main.rs
@@ -523,33 +523,37 @@ fn get_classification(
             return PeerClassification::GenericBad;
         }
 
+        // AllGood test section
         let ewmas = peer_stats.ewma_pack;
         if peer_stats.total_attempts <= 3 && peer_stats.valid_block_reply_ok * 2 >= peer_stats.total_attempts
         {
-            return PeerClassification::AllGood;
-        };
+            return ancillary_checks_all_good(&peer_stats, network);
+        }
 
         if ewmas.stat_2_hours.reliability > 0.85 && ewmas.stat_2_hours.count > 2.0 {
-            return PeerClassification::AllGood;
-        };
+            return ancillary_checks_all_good(&peer_stats, network);
+        }
         if ewmas.stat_8_hours.reliability > 0.70 && ewmas.stat_8_hours.count > 4.0 {
-            return PeerClassification::AllGood;
-        };
+            return ancillary_checks_all_good(&peer_stats, network);
+        }
         if ewmas.stat_1day.reliability > 0.55 && ewmas.stat_1day.count > 8.0 {
-            return PeerClassification::AllGood;
-        };
+            return ancillary_checks_all_good(&peer_stats, network);
+        }
         if ewmas.stat_1week.reliability > 0.45 && ewmas.stat_1week.count > 16.0 {
-            return PeerClassification::AllGood;
-        };
+            return ancillary_checks_all_good(&peer_stats, network);
+        }
         if ewmas.stat_1month.reliability > 0.35 && ewmas.stat_1month.count > 32.0 {
-            return PeerClassification::AllGood;
-        };
-
-        // at least one success in the last 2 hours
+            return ancillary_checks_all_good(&peer_stats, network);
+        }
+        // MerelySyncedEnough test section
+        // if it doesn't meet the uptime criteria but it passed the blocks test in the past 2 hours, serve it as an alternate
         if let Some(last_success) = peer_stats.last_success {
             if let Ok(duration) = last_success.elapsed() {
                 if duration <= Duration::from_secs(60 * 60 * 2) {
-                    return PeerClassification::MerelySyncedEnough;
+                    return ancillary_checks_merely_synced(&peer_stats, network);
+                }
+            }
+        }
                 }
             }
         }
