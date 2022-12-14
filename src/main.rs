e712ad32ff44fff65a13da439c308694c3ad115a
sasha <sasha@electriccoin.co>
2022-12-14 15:06:40 -0800

    start work on a more lightweight probe type

--- src/main.rs
+++ src/main.rs
@@ -497,13 +497,13 @@ async fn hash_probe_inner(
 
 
 
-async fn connection_probe_inner(
+async fn lightweight_probe_inner(
     peer_addr: SocketAddr,
     network: Network,
     connection_timeout: Duration,
-) -> BlockProbeResult {
+) -> LightweightProbeResult {
     println!(
-        "Starting new hash probe connection: peer addr is {:?}",
+        "Starting new lightweight probe connection: peer addr is {:?}",
         peer_addr
     );
     let connection = connect_isolated_tcp_direct(
@@ -518,14 +518,16 @@ async fn connection_probe_inner(
         Network::Mainnet => HASH_CHECKPOINTS_MAINNET.clone(),
         Network::Testnet => HASH_CHECKPOINTS_TESTNET.clone(),
     };
+    let headers_vec = Vec::from_iter(hash_checkpoints);
+    println!("headers vec {:?}", headers_vec);
 
     match connection {
         Err(timeout_error) => {
             println!(
-                "Hash test connection with {:?} failed due to user-defined timeout of {:?}: {:?}",
+                "Lightweight test connection with {:?} failed due to user-defined timeout of {:?}: {:?}",
                 peer_addr, connection_timeout, timeout_error
             );
-            BlockProbeResult::TCPFailure // this counts as network brokenness, should not count as BeyondUseless, just regular Bad
+            LightweightProbeResult::TCPFailure // this counts as network brokenness, should not count as BeyondUseless, just regular Bad
         }
         Ok(connection_might_have_failed) => {
             match connection_might_have_failed {
@@ -535,24 +537,24 @@ async fn connection_probe_inner(
                     match error_classification {
                         ErrorFlavor::Ephemeral(msg) => {
                             println!(
-                                "Hash test connection with {:?} got an ephemeral error: {:?}",
+                                "Lightweight test connection with {:?} got an ephemeral error: {:?}",
                                 peer_addr, msg
                             );
-                            BlockProbeResult::MustRetry
+                            LightweightProbeResult::MustRetry
                         }
                         ErrorFlavor::Network(msg) => {
                             println!(
-                                "Hash test connection with {:?} got an network error: {:?}",
+                                "Lightweight test connection with {:?} got an network error: {:?}",
                                 peer_addr, msg
                             );
-                            BlockProbeResult::TCPFailure
+                            LightweightProbeResult::TCPFailure
                         }
                         ErrorFlavor::Protocol(msg) => {
                             println!(
-                                "Hash test connection with {:?} got an protocol error: {:?}",
+                                "Lightweight test connection with {:?} got an protocol error: {:?}",
                                 peer_addr, msg
                             );
-                            BlockProbeResult::ProtocolBad
+                            LightweightProbeResult::ProtocolBad
                         }
                     }
                 }
@@ -572,50 +574,26 @@ async fn connection_probe_inner(
                     // println!("remote peer version: {:?}", z.connection_info.remote.version >= Version(170_100));
                     // println!("remote peer services: {:?}", z.connection_info.remote.services.intersects(PeerServices::NODE_NETWORK));
                     // println!("remote peer height @ time of connection: {:?}", z.connection_info.remote.start_height >= Height(1_700_000));
-
-                    let hash_query_response = good_connection
-                        .call(Request::BlocksByHash(hash_checkpoints.clone()))
+                    let headers_request = Request::FindHeaders {known_blocks: headers_vec, stop: None};
+                    let headers_query_response = good_connection
+                        .call(headers_request.clone())
                         .await;
-                    //println!("hash query response is {:?}", hash_query_response);
-                    match hash_query_response {
+                    //println!("headers query response is {:?}", headers_query_response);
+                    match headers_query_response {
                         Err(protocol_error) => {
-                            println!("protocol failure after requesting blocks by hash with peer {}: {:?}", peer_addr, protocol_error);
+                            println!("protocol failure after requesting headers with peer {}: {:?}", peer_addr, protocol_error);
                             // so far the only errors we've seen here look like one of these, and it's fine to ascribe them to BlockRequestFail and not to ProtocolBad
                             // SharedPeerError(ConnectionClosed)
                             // SharedPeerError(ConnectionReceiveTimeout)
                             // SharedPeerError(NotFoundResponse([Block(block::Hash("")), Block(block::Hash(""))]))
-                            return BlockProbeResult::BlockRequestFail(peer_derived_data);
+                            return LightweightProbeResult::LightweightFail(peer_derived_data);
                         }
-                        Ok(hash_query_protocol_response) => {
-                            match hash_query_protocol_response {
-                                Response::Blocks(block_vector) => {
-                                    let mut returned_hashes = HashSet::new();
-                                    for block_k in block_vector {
-                                        if let InventoryResponse::Available(actual_block) = block_k
-                                        {
-                                            returned_hashes.insert(actual_block.hash());
-                                        }
-                                    }
-                                    //println!("{:?}", returned_hashes);
-                                    let intersection_count =
-                                        returned_hashes.intersection(&hash_checkpoints).count();
-                                    // println!("intersection_count is {:?}", intersection_count);
-                                    if intersection_count == hash_checkpoints.len() {
-                                        // All requested blocks are there and hash OK
-                                        println!("Peer {:?} returned good hashes", peer_addr);
-                                        return BlockProbeResult::BlockRequestOK(peer_derived_data);
-                                    }
-                                    // node returned a Blocks response, but it wasn't complete/correct for some reason
-                                    BlockProbeResult::BlockRequestFail(peer_derived_data)
-                                } // Response::Blocks(block_vector)
-                                _ => {
-                                    // connection established but we didn't get a Blocks response
-                                    BlockProbeResult::BlockRequestFail(peer_derived_data)
-                                }
-                            } // match hash_query_protocol_response
-                        } // Ok(hash_query_protocol_response)
-                    } // match hash_query_response
-                } // Ok(good_connection)
+                        Ok(headers_query_protocol_response) => {
+                            println!("headers request gave us {:?}", headers_query_protocol_response);
+                            return LightweightProbeResult::LightweightOK(peer_derived_data);
+                        }
+                    }
+                }
             }
         }
     }
@@ -721,10 +699,28 @@ enum BlockProbeResult {
     ProtocolBad, // increment total_attempts and tcp_connections_ok
 
     // Abnormal reply to the block request. We negotiated the protocol OK, but their reply to the block requests wasn't perfect
-    BlockRequestFail(PeerDerivedData), // increment total_attempts and tcp_connections_ok and protocol_negotations_ok
+    BlockRequestFail(PeerDerivedData), // increment total_attempts and tcp_connections_ok and protocol_negotiations_ok
 
     // Nominal reply to the block request
     BlockRequestOK(PeerDerivedData), // increment total_attempts and tcp_connections_ok and protocol_negotations_ok and valid_block_reply_ok
+}
+
+#[derive(Debug, Clone)]
+enum LightweightProbeResult {
+    // Error on our host (too many FDs, etc), retryable and says nothing about the host or the network
+    MustRetry, // increment nothing
+
+    // Error establishing the TCP connection. Counts as a normal failure against the node
+    TCPFailure, // increment total_attempts
+
+    // Protocol negotiation failure. We were able to establish a TCP connection, but they said something zebra-network didn't like
+    ProtocolBad, // increment total_attempts and tcp_connections_ok
+
+    // Abnormal reply to the headers request. We negotiated the protocol OK, but their reply to the block requests wasn't perfect
+    LightweightFail(PeerDerivedData), // increment total_attempts and tcp_connections_ok and protocol_negotiations_ok
+
+    // Nominal reply to the headers request
+    LightweightOK(PeerDerivedData), // increment total_attempts and tcp_connections_ok and protocol_negotations_ok and valid_headers_reply_ok
 }
 
 #[derive(Debug, Clone)]
