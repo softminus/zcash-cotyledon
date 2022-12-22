0410fb70b04e1a7a685cb9307f172097ab8980ae
sasha <sasha@electriccoin.co>
2022-12-22 12:58:56 -0800

    pass a ProbeConfiguration to get_classification where it's called

--- src/serving.rs
+++ src/serving.rs
@@ -9,7 +9,7 @@ use zebra_chain::parameters::Network;
 
 use crate::probe::classify::{get_classification, PeerStats};
 use crate::probe::PeerClassification;
-
+use crate::probe::classify::ProbeConfiguration;
 #[derive(Debug, Clone, Default)]
 pub struct ServingNodes {
     pub primaries: HashSet<SocketAddr>,
@@ -19,13 +19,14 @@ pub struct ServingNodes {
 pub fn update_serving_nodes(
     serving_nodes_shared: &Arc<RwLock<ServingNodes>>,
     internal_peer_tracker: &HashMap<SocketAddr, Option<PeerStats>>,
+    probes_config: &ProbeConfiguration
 ) {
     let mut primary_nodes = HashSet::new();
     let mut alternate_nodes = HashSet::new();
 
     for (key, value) in internal_peer_tracker {
         if key.ip().is_global() {
-            let classification = get_classification(&value, &key, Network::Mainnet);
+            let classification = get_classification(&value, &key, Network::Mainnet, probes_config);
             if classification == PeerClassification::AllGood {
                 primary_nodes.insert(key.clone());
             }
@@ -49,6 +50,7 @@ pub fn single_node_update(
     serving_nodes_shared: &Arc<RwLock<ServingNodes>>,
     new_peer_address: &SocketAddr,
     new_peer_stat: &Option<PeerStats>,
+    probes_config: &ProbeConfiguration
 ) {
     if new_peer_address.ip().is_global() {
         let old_nodes = serving_nodes_shared.read().unwrap();
@@ -56,7 +58,7 @@ pub fn single_node_update(
         let mut alternate_nodes = old_nodes.alternates.clone();
         drop(old_nodes);
 
-        match get_classification(new_peer_stat, new_peer_address, Network::Mainnet) {
+        match get_classification(new_peer_stat, new_peer_address, Network::Mainnet, probes_config) {
             PeerClassification::AllGood => {
                 primary_nodes.insert(new_peer_address.clone());
                 alternate_nodes.remove(new_peer_address);
