60d169519754ffae03eb663ca6542ae295a53cc1
sasha <sasha@electriccoin.co>
2022-12-22 12:33:48 -0800

    start refactor for classification

--- src/probe/classify.rs
+++ src/probe/classify.rs
@@ -46,9 +46,151 @@ pub enum AugmentedProbeTypes {
 
 
 pub struct ProbeConfiguration {
-    ewma_probe: AugmentedProbeTypes,
-    gating_probes: Vec<AugmentedProbeTypes>
+    ewma_probe: ProbeType,
+    all_good_gating_probes: Vec<AugmentedProbeTypes>,
+    merely_synced_gating_probes: Vec<AugmentedProbeTypes>
+    eventually_maybe_gating_probes: Vec<AugmentedProbeTypes>
+
 }
+
+
+pub fn all_good_test(peer_stats: &Option<PeerStats>, network: Network, probes_config: ProbeConfiguration) -> bool {
+    if let Some(peer_derived_data) = peer_stats.peer_derived_data.as_ref() {
+        // negotiating the protocol at least once rules out Unknown and BeyondUseless
+        // therefore, the node is one of {AllGood, MerelySyncedEnough, EventuallyMaybeSynced, GenericBad}
+        // we test in the order of decreasing stringency, and return whenever we meet all the criteria for a condition
+
+        // for AllGood, we need to pass the uptime criteria and (peer_services test, numeric version test, peer_height test)
+        // for MerelySyncedEnough, we merely need a good block test in the past 2 hours and (peer_services test, numeric version test, peer_height test)
+        // for EventuallyMaybeSynced, we need to have passed (peer_services test, numeric version test) in 24 hours, but peer_height test can fail
+        // otherwise, we give a GenericBad
+
+        // AllGood test section
+        let ewmas = peer_stats.ewma_pack;
+
+        let probe_stat = match probes_config.ewma_probe {
+            ProbeType::Block => peer_stats.block_probe,
+            ProbeType::Headers => peer_stats.header_probe,
+            ProbeType::Negotiation => peer_stats.protocol_negotiation,
+
+        }
+        if probe_stat.total_attempts <= 3
+            && probe_stat.valid_block_reply_ok * 2 >= probe_stat.total_attempts
+        {
+            return true;
+        }
+        if ewmas.stat_2_hours.reliability > 0.85 && ewmas.stat_2_hours.count > 2.0 {
+            return true;
+        }
+        if ewmas.stat_8_hours.reliability > 0.70 && ewmas.stat_8_hours.count > 4.0 {
+            return true;
+        }
+        if ewmas.stat_1day.reliability > 0.55 && ewmas.stat_1day.count > 8.0 {
+            return true;
+        }
+        if ewmas.stat_1week.reliability > 0.45 && ewmas.stat_1week.count > 16.0 {
+            return true;
+        }
+        if ewmas.stat_1month.reliability > 0.35 && ewmas.stat_1month.count > 32.0 {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+pub fn get_classification(
+    peer_stats: &Option<PeerStats>,
+    peer_address: &SocketAddr,
+    network: Network,
+    probes_config: ProbeConfiguration
+) -> PeerClassification {
+
+    // generic logic, rule out never having attempted this peer
+    let peer_stats = match peer_stats {
+        None => return PeerClassification::Unknown,
+        Some(peer_stats) => peer_stats,
+    };
+
+    // generic logic, rule out never having attempted this peer
+    if peer_stats.tcp_connection.attempt_count == 0 {
+        // we never attempted to connect to this peer
+        return PeerClassification::Unknown;
+    }
+
+    if all_good_test(peer_stats, network, probes_config) {
+        if check_gating(peer_stats, network, probes_config.all_good_gating_probes) {
+            return PeerClassification::AllGood;
+        }
+    }
+
+    if merely_synced_test(peer_stats, network, probes_config) {
+        if check_gating(peer_stats, network, probes_config.merely_synced_gating_probes) {
+            return PeerClassification::MerelySyncedEnough;
+        }
+    }
+
+    if eventually_maybe_test(peer_stats, network, probes_config) {
+        if check_gating(peer_stats, network, probes_config.eventually_maybe_gating_probes) {
+            return PeerClassification::EventuallyMaybeSynced;
+        }
+    }
+
+    if beyond_useless_test(peer_stats, network, probes_config) {
+        return PeerClassification::BeyondUseless;
+    }
+
+    return PeerClassification::GenericBad;
+}
+
+
+
+        // MerelySyncedEnough test section
+        // if it doesn't meet the uptime criteria but it passed the blocks test in the past 2 hours, serve it as an alternate
+        if let Some(last_block_success) = peer_stats.last_block_success {
+            if let Ok(duration) = last_block_success.elapsed() {
+                if duration <= Duration::from_secs(60 * 60 * 2) {
+                    return ancillary_checks_merely_synced(
+                        peer_derived_data,
+                        peer_address,
+                        peer_stats,
+                        network,
+                    );
+                }
+            }
+        }
+
+        // EventuallyMaybeSynced test section
+        // if last protocol negotiation was more than 24 hours ago, this is not worth special attention, keep polling it at the slower rate
+        if let Some(last_protocol_negotiation) = peer_stats.last_protocol_negotiation {
+            if let Ok(duration) = last_protocol_negotiation.elapsed() {
+                if duration <= Duration::from_secs(60 * 60 * 24) {
+                    return ancillary_checks_eventually_maybe_synced(
+                        peer_derived_data,
+                        peer_address,
+                        peer_stats,
+                        network,
+                    );
+                }
+            }
+        }
+
+        // GenericBad test section
+        println!("WARNING: classifying node {:?} with PeerStats {:?} as GenericBad despite having negotiated wire protocol: {:?}", peer_address, peer_stats, peer_derived_data);
+        return PeerClassification::GenericBad;
+    } else {
+        // never were able to negotiate the wire protocol
+        if peer_stats.tcp_connections_ok > 10 {
+            // at least 10 TCP connections succeeded, but never been able to negotiate the Zcash protocol
+            // this isn't a zcash node and isn't going to turn into one any time soon
+            return PeerClassification::BeyondUseless;
+        } else {
+            // need more samples before hitting it with the worst possible penalty
+            return PeerClassification::GenericBad;
+        }
+    }
+}
+
 
 
 fn ancillary_checks_all_good(
@@ -107,99 +249,8 @@ fn ancillary_checks_eventually_maybe_synced(
     }
 }
 
-pub fn get_classification(
-    peer_stats: &Option<PeerStats>,
-    peer_address: &SocketAddr,
-    network: Network,
-) -> PeerClassification {
-    let peer_stats = match peer_stats {
-        None => return PeerClassification::Unknown,
-        Some(peer_stats) => peer_stats,
-    };
 
-    if peer_stats.total_attempts == 0 {
-        // we never attempted to connect to this peer
-        return PeerClassification::Unknown;
-    }
 
-    if let Some(peer_derived_data) = peer_stats.peer_derived_data.as_ref() {
-        // negotiating the protocol at least once rules out Unknown and BeyondUseless
-        // therefore, the node is one of {AllGood, MerelySyncedEnough, EventuallyMaybeSynced, GenericBad}
-        // we test in the order of decreasing stringency, and return whenever we meet all the criteria for a condition
-
-        // for AllGood, we need to pass the uptime criteria and (peer_services test, numeric version test, peer_height test)
-        // for MerelySyncedEnough, we merely need a good block test in the past 2 hours and (peer_services test, numeric version test, peer_height test)
-        // for EventuallyMaybeSynced, we need to have passed (peer_services test, numeric version test) in 24 hours, but peer_height test can fail
-        // otherwise, we give a GenericBad
-
-        // AllGood test section
-        let ewmas = peer_stats.ewma_pack;
-        if peer_stats.total_attempts <= 3
-            && peer_stats.valid_block_reply_ok * 2 >= peer_stats.total_attempts
-        {
-            return ancillary_checks_all_good(peer_derived_data, peer_address, peer_stats, network);
-        }
-        if ewmas.stat_2_hours.reliability > 0.85 && ewmas.stat_2_hours.count > 2.0 {
-            return ancillary_checks_all_good(peer_derived_data, peer_address, peer_stats, network);
-        }
-        if ewmas.stat_8_hours.reliability > 0.70 && ewmas.stat_8_hours.count > 4.0 {
-            return ancillary_checks_all_good(peer_derived_data, peer_address, peer_stats, network);
-        }
-        if ewmas.stat_1day.reliability > 0.55 && ewmas.stat_1day.count > 8.0 {
-            return ancillary_checks_all_good(peer_derived_data, peer_address, peer_stats, network);
-        }
-        if ewmas.stat_1week.reliability > 0.45 && ewmas.stat_1week.count > 16.0 {
-            return ancillary_checks_all_good(peer_derived_data, peer_address, peer_stats, network);
-        }
-        if ewmas.stat_1month.reliability > 0.35 && ewmas.stat_1month.count > 32.0 {
-            return ancillary_checks_all_good(peer_derived_data, peer_address, peer_stats, network);
-        }
-
-        // MerelySyncedEnough test section
-        // if it doesn't meet the uptime criteria but it passed the blocks test in the past 2 hours, serve it as an alternate
-        if let Some(last_block_success) = peer_stats.last_block_success {
-            if let Ok(duration) = last_block_success.elapsed() {
-                if duration <= Duration::from_secs(60 * 60 * 2) {
-                    return ancillary_checks_merely_synced(
-                        peer_derived_data,
-                        peer_address,
-                        peer_stats,
-                        network,
-                    );
-                }
-            }
-        }
-
-        // EventuallyMaybeSynced test section
-        // if last protocol negotiation was more than 24 hours ago, this is not worth special attention, keep polling it at the slower rate
-        if let Some(last_protocol_negotiation) = peer_stats.last_protocol_negotiation {
-            if let Ok(duration) = last_protocol_negotiation.elapsed() {
-                if duration <= Duration::from_secs(60 * 60 * 24) {
-                    return ancillary_checks_eventually_maybe_synced(
-                        peer_derived_data,
-                        peer_address,
-                        peer_stats,
-                        network,
-                    );
-                }
-            }
-        }
-
-        // GenericBad test section
-        println!("WARNING: classifying node {:?} with PeerStats {:?} as GenericBad despite having negotiated wire protocol: {:?}", peer_address, peer_stats, peer_derived_data);
-        return PeerClassification::GenericBad;
-    } else {
-        // never were able to negotiate the wire protocol
-        if peer_stats.tcp_connections_ok > 10 {
-            // at least 10 TCP connections succeeded, but never been able to negotiate the Zcash protocol
-            // this isn't a zcash node and isn't going to turn into one any time soon
-            return PeerClassification::BeyondUseless;
-        } else {
-            // need more samples before hitting it with the worst possible penalty
-            return PeerClassification::GenericBad;
-        }
-    }
-}
 
 fn required_height(network: Network) -> Height {
     match network {
